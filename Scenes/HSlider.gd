2f7de40eaa74e3661f41738fba228b9b09dc8a23
andrebariani <andrebariani@gmail.com>
2021-10-04 17:51:56 -0300

    volume controls

--- Scenes/HSlider.gd
+++ Scenes/HSlider.gd
@@ -5,15 +5,19 @@ var original_sfx = AudioServer.get_bus_volume_db(2)
 var original_bgm = AudioServer.get_bus_volume_db(1)
 
 func _ready():
+	match group:
+		'SFX': value = Global.sound_volume
+		'BGM': value = Global.music_volume
 	self.connect("value_changed", self, "_on_value_changed")
 
 
 func _on_value_changed(value):
-	print_debug("miau")
 	match group:
 		'SFX':
-			print_debug(value - max_value) 
+			print_debug((value) - max_value)
 			AudioServer.set_bus_volume_db(2, value - max_value)
+			Global.sound_volume = value
 		'BGM':
-			print_debug(value - max_value)
+			print_debug((value) - max_value)
 			AudioServer.set_bus_volume_db(1, value - max_value)
+			Global.music_volume = value
