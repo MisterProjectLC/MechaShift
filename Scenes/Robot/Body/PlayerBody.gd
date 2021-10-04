674d811ba9631f2dd763c154cfed18652fbceaab
Anderson <ander.user@gmail.com>
2021-10-03 23:17:31 -0300

    Animação robô andando

---
+++ Scenes/Robot/Body/PlayerBody.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+onready var robot = get_parent().get_parent()
+
+func _process(delta):
+	if robot:
+		_update_animation()
+
+
+func _update_animation():
+	var current_force = (robot.Wheel as RigidBody2D).angular_velocity
+	if current_force > 0:
+		scale.x = 1
+		$AnimationPlayer.play("run")
+	elif current_force < 0:
+		scale.x = -1
+		$AnimationPlayer.play("run")
+	else:
+		$AnimationPlayer.play("idle")
+
+
+func play_animation(animation):
+	$AnimationPlayer.play(animation)
